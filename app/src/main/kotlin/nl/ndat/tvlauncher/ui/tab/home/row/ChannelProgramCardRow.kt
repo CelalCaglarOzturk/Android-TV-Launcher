c3a4ef9c69b0ca1defbd24ebb7775460eb9bb41a
Celal Çağlar Öztürk <celalcaglar34@gmail.com>
2026-01-23 19:11:02 +0300

    cahnnel text fix

--- app/src/main/kotlin/nl/ndat/tvlauncher/ui/tab/home/row/ChannelProgramCardRow.kt
+++ app/src/main/kotlin/nl/ndat/tvlauncher/ui/tab/home/row/ChannelProgramCardRow.kt
@@ -9,7 +9,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -124,7 +124,7 @@ fun ChannelProgramCardRow(
                             horizontalArrangement = Arrangement.spacedBy(14.dp),
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .height(baseHeight + 8.dp)
+                                .heightIn(min = baseHeight + 8.dp)
                                 .focusRestorer(childFocusRequester),
                         ) {
                             itemsIndexed(
