52d59c6a6aac9ac1407c0aec1ce0ef70b2816085
Celal Çağlar Öztürk <celalcaglar34@gmail.com>
2026-01-28 20:27:04 +0300

    android 9+ comaptability

--- app/src/main/kotlin/nl/ndat/tvlauncher/util/DefaultLauncherHelper.kt
+++ app/src/main/kotlin/nl/ndat/tvlauncher/util/DefaultLauncherHelper.kt
@@ -4,25 +4,44 @@ import android.annotation.SuppressLint
 import android.app.role.RoleManager
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
+import android.provider.Settings
 import androidx.core.content.getSystemService
 import androidx.core.role.RoleManagerCompat
 
 class DefaultLauncherHelper(
-	context: Context,
+    private val context: Context,
 ) {
-	private val roleManager by lazy { context.getSystemService<RoleManager>() }
-	private val isCompatible = Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q && roleManager != null
+    // Only accessed on API 29+
+    private val roleManager by lazy { context.getSystemService<RoleManager>() }
 
-	@SuppressLint("NewApi")
-	fun isDefaultLauncher(): Boolean = isCompatible && roleManager!!.isRoleHeld(RoleManagerCompat.ROLE_HOME)
+    @SuppressLint("NewApi")
+    fun isDefaultLauncher(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            roleManager != null && roleManager!!.isRoleHeld(RoleManagerCompat.ROLE_HOME)
+        } else {
+            val intent = Intent(Intent.ACTION_MAIN).addCategory(Intent.CATEGORY_HOME)
+            val resolveInfo = context.packageManager.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY)
+            resolveInfo?.activityInfo?.packageName == context.packageName
+        }
+    }
 
-	@SuppressLint("NewApi")
-	fun canRequestDefaultLauncher(): Boolean =
-		isCompatible && roleManager!!.isRoleAvailable(RoleManagerCompat.ROLE_HOME)
+    @SuppressLint("NewApi")
+    fun canRequestDefaultLauncher(): Boolean {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            roleManager != null && roleManager!!.isRoleAvailable(RoleManagerCompat.ROLE_HOME)
+        } else {
+            true
+        }
+    }
 
-	@SuppressLint("NewApi")
-	fun requestDefaultLauncherIntent(): Intent? =
-		if (isCompatible) roleManager!!.createRequestRoleIntent(RoleManagerCompat.ROLE_HOME)
-		else null
+    @SuppressLint("NewApi")
+    fun requestDefaultLauncherIntent(): Intent? {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            roleManager?.createRequestRoleIntent(RoleManagerCompat.ROLE_HOME)
+        } else {
+            Intent(Settings.ACTION_HOME_SETTINGS)
+        }
+    }
 }
